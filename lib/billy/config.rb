ce97d7297a90d21b9c1a777fbb3bff2379efdae8
4pcbr <i4pcbr@gmail.com>
2012-11-26 20:57:11 +0400

    Config could be stringified

--- lib/billy/config.rb
+++ lib/billy/config.rb
@@ -1,20 +1,39 @@
 class Billy
   class Config
     
+    BILLYRC = '.billyrc'
+    
     attr_accessor :storage
     
-    def method_missing( m, args, &block )
+    def method_missing( m, *args, &block )
       if m.to_s[ /=$/ ].nil?
         # get method
-        self.storage[ m.to_sym ]
+        self.storage[ m.to_s ]
       else
         # set method
-        self.storage[ m.to_sym ] = args.shift
+        self.storage[ ( m.to_s )[ /^([^=]+)/ ] ] = args.shift
       end
     end
     
-    def save!( path_to_save )
+    def save!( dir )
       # TODO: save it
+      raise 'Directory name should not be empty' unless !dir.empty?
+      raise "Directory #{dir.to_s} doesn't exist" unless File.exist?( File.expand_path( dir ) )
+      billyrc_path = File.expand_path( dir_to_save + "/#{BILLYRC}" )
+      billyrc_file = File.new( billyrc_path )
+      raise "Config already exists in #{billyrc_path}" unless !config_exists?( dir )
+    end
+    
+    def config_exists?( dir )
+      File.exists?( File.expand_path( dir + "/#{BILLYRC}" ) )
+    end
+
+    def to_s
+      [].tap { |res|
+        self.storage.each_pair do |k, v|
+          res.push "#{k}\t#{v}"
+        end
+      }.push( "" ).join( "\n" )
     end
 
     protected
--- spec/lib/billy/config_spec.rb
+++ spec/lib/billy/config_spec.rb
@@ -4,12 +4,68 @@ describe Billy::Config do
   
   describe 'Config.instance' do
     
-    it 'doesn\'t throw error' do
+    it 'Should not throw error' do
       expect{ Billy::Config.instance }.to_not raise_error
     end
     
-    it 'instantiates itself' do
+    it 'Should instantiates itself' do
       Billy::Config.instance.should be_an_instance_of( Billy::Config )
+    end
+    
+    it 'Should return same instance' do
+      i1 = Billy::Config.instance
+      i2 = Billy::Config.instance
+      ( i1.equal?( i2 ) ).should be_true
+    end
+    
+  end
+  
+  describe 'Storage' do
+    
+    let!( :unknown_key ) { 'asdasd12091823#$%^&3' }
+    let!( :known_key ) { 'qweq123-&65poi!@#$' }
+    let!( :known_value ) { 'asdas#$%^&3' }
+    
+    it 'Should not throw error on unknown method call' do
+      Billy::Config.instance.send( unknown_key )
+      expect{ Billy::Config.instance.send( unknown_key ) }.to_not raise_error
+    end
+    
+    it 'Should return nil for unknown key' do
+      Billy::Config.instance.send( unknown_key ).should be_nil
+    end
+    
+    it 'Should not raise error on new value set' do
+      expect{ Billy::Config.instance.send( "#{known_key}=", known_value ) }.to_not raise_error
+    end
+    
+    it 'Should save value' do
+      Billy::Config.instance.send( "#{known_key}=", known_value )
+      Billy::Config.instance.send( known_key ).should eq known_value
+    end
+    
+  end
+  
+  describe 'File' do
+    
+    let!( :test_dir ) { File.expand_path( File.dirname( __FILE__ ) + "../../../../tmp/" ) }
+    let!( :billy ) { Billy::Config.instance }
+    let!( :known_key1 ) { 'qweq123-&65poi!@#$' }
+    let!( :known_value1 ) { 'asdas#$%^&3' }
+    let!( :known_key2 ) { 'poioiuyuytrdfa!@#$' }
+    let!( :known_value2 ) { 'mnsdgfkgwu#$%^&3' }
+    
+    it 'Should return false if config doesn\'t exists' do
+      billy.config_exists?( test_dir ).should be_false
+    end
+    
+    it 'Should stingify itself' do
+      billy.send( "#{known_key1}=", known_value1 )
+      billy.send( "#{known_key2}=", known_value2 )
+      billy.to_s.should eq <<-EOS
+#{known_key1}\t#{known_value1}
+#{known_key2}\t#{known_value2}
+EOS
     end
     
   end
