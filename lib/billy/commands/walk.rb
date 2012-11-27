d6bfc1e6d826e6158f5323897af63e12b1faff09
Sergei Koniukhovskiy <sk@onthe.net.ru>
2012-11-27 20:42:51 +0400

    started walking

--- lib/billy/commands/walk.rb
+++ lib/billy/commands/walk.rb
@@ -1,12 +1,33 @@
 require 'billy/commands/command'
+require 'capistrano'
 
 class Billy
   class Commands
     class Walk < Command
-      
-      def proceed!( arguments )
-        
+
+      def proceed!(arguments)
+        destination = arguments[0]
+
+        cap = prepare_capistrano(destination)
+
+        #deploy here
       end
+
+      def prepare_capistrano(destination) 
+        cap = Capistrano::Configuration.new
+        config = Billy::Config.instance
+
+        cap.set :scm, "git"
+        cap.set :deploy_via, :remote_cache
+        cap.set :deploy_path, File.join(config.remote_path,destination)
+
+        [:user,:repository,:deploy_path].each do |variable|
+          cap.set variable, config.send(variable)
+        end
+
+        cap
+      end
+
       
     end
   end
--- spec/lib/billy/commands/walk_spec.rb
+++ spec/lib/billy/commands/walk_spec.rb
@@ -1,5 +1,50 @@
 require 'billy/commands/walk'
 
 describe Billy::Commands::Walk do
+  let!( :command ) { Billy::Commands::Walk.instance }
   
+  describe :prepare_capistrano do
+    context 'With correct config' do
+      let(:destination) { "test_destination"}
+      before :each do
+        Billy::Config.any_instance.stub(
+          :user => "nobody",
+          :remote_path => "/tmp/",
+          :repository => "git://some.repo.com/"
+          )
+
+      end
+
+      it 'Should not fail' do
+        expect { command.prepare_capistrano(destination) }.not_to raise_error
+      end
+
+      it 'Should return capistrano instance' do
+        command.prepare_capistrano(destination).should be_an_instance_of(Capistrano::Configuration)
+      end
+    end
+
+    context 'Without destination' do
+      before :each do
+        Billy::Config.any_instance.stub(
+          :user => "nobody",
+          :remote_path => "/tmp/",
+          :repository => "git://some.repo.com/"
+          )
+
+      end
+      it 'Should fail' do
+        expect { command.prepare_capistrano }.to raise_error
+        expect { command.prepare_capistrano(nil) }.to raise_error
+      end
+    end
+
+    context 'Without  config' do
+      let(:destination) { "test_destination"}
+      it 'Should fail' do
+        expect { command.prepare_capistrano(destination) }.to raise_error
+      end
+    end
+
+  end    
 end
