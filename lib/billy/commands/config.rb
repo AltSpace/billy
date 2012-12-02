c15455472cdf2641b404e4c56480c7dadf47acaf
4pcbr <me@4pcbr.com>
2012-12-03 00:58:46 +0400

    config command loads not current folder config file only but home folder as well

--- lib/billy/commands/config.rb
+++ lib/billy/commands/config.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 require 'billy/commands/command'
 require 'billy/util/ui'
 
@@ -6,17 +7,22 @@ module Billy
     class Config < Command
       
       def proceed!( arguments = nil )
-        dir = Dir.pwd
-        if !Billy::Config.config_exists?( dir )
-          Billy::Util::UI.err "Billy config file could not be found here. Say Billy hello."
-          exit 1
-        else
+        %w(. ~).each do |f|
+          dir = File.expand_path( f )
+          next unless Billy::Config.config_exists?( dir )
+          Billy::Util::UI.inform "Config loaded from file: #{dir}/#{Billy::Config::BILLYRC}"
           Billy::Config.load!( dir )
           Billy::Util::UI.inform "Current Billy settings:"
           Billy::Config.settings.each_pair do |k, v|
-            Billy::Util::UI.inform "#{k}:\t\t\t#{v}"
+            yellow_k = "#{k}".yellow
+            Billy::Util::UI.inform "#{yellow_k}:\t\t#{v.blue}"
           end
+          return
         end
+        
+        Billy::Util::UI.err "Billy config file could not be found here. Say Billy hello."
+        exit 1
+        
       end
       
     end
